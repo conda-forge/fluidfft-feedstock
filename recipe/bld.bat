01cb1a4dd199f9cd5f82065f8d336d327c284437
Cyrille Bonamy <cyrille.bonamy@univ-grenoble-alpes.fr>
2019-05-01 00:21:16 +0200

    try debug

---
+++ recipe/bld.bat
@@ -0,0 +1,36 @@
+setlocal EnableDelayedExpansion
+
+echo [fftw3]^
+use = True^
+dir =^
+include_dir =^
+library_dir =^
+^
+[fftw3_mpi]^
+use = False^
+dir =^
+include_dir =^
+library_dir =^
+^
+[cufft]^
+use = False^
+dir =^
+include_dir =^
+library_dir =^
+^
+[pfft]^
+use = False^
+dir =^
+include_dir =^
+library_dir =^
+^
+[p3dfft]^
+use = False^
+dir =^
+include_dir =^
+library_dir =^
+^
+> site.cfg
+
+
+$PYTHON -m pip install --no-deps --ignore-installed -vv .[full]
