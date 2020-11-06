3fd7f671b0011a3e2969ec1c952deb1b804e13f7
Ashwin Vishnu <9155111+ashwinvis@users.noreply.github.com>
2020-11-06 15:36:26 +0100

    [ci skip] ***NO_CI*** mention issue and rm noise

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,11 +1,8 @@
 set DISABLE_PYTHRAN=1
 
-rem When Cython extensions are fixed, uncomment the following
+rem When Cython extensions are fixed, uncomment the following. See https://foss.heptapod.net/fluiddyn/fluidfft/-/issues/29
 rem COPY %RECIPE_DIR%\site_nompi.cfg .\site.cfg
 rem if errorlevel 1 exit 1
-
-"%PYTHON%" -m pip install .[fftw] --no-deps -vv
-if errorlevel 1 exit 1
 
 "%PYTHON%" -m pip install .[fftw] --no-deps -vv
 if errorlevel 1 exit 1
